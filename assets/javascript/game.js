bccc8b0b0873be02a46004e1c58407d4b2d72b62
Elephant_Man <grogan.charlie@gmail.com>
2018-08-07 01:21:26 -0500

    pseudo code for game written

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//cpuPick = random # between (19-120)
+
+
+//each crystal is assigned a random # between 1-12 hidden from //player view
+
+
+//when player clicks each button the value that was assigned is //added to the total score 
+
+
+//(playerWin = playerScore === cpuPick)
+
+
+//if player win - ++ win counter 
+
+
+//cpu picks new number
+
+
+//crystals assigned new value
+
+
+//write new score to html
+
+
+//(playerLoss = playerScore > cpuPick)
+
+
+//if player losses ++ loss counter
+
+
+//cpu picks new number
+
+
+//crystals assigned new value
+
+
+//write new score to html
+
+
+
+
