347e9661dadb13e4930fefe4c7158e51cd67c554
Elephant_Man <grogan.charlie@gmail.com>
2018-08-07 02:05:08 -0500

    styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,59 @@
+//------------------------------------------------------------------
+//________start the game____________________________________________
 //cpuPick = random # between (19-120)
+
 
 
 //each crystal is assigned a random # between 1-12 hidden from //player view
 
 
+
 //when player clicks each button the value that was assigned is //added to the total score 
 
 
+
+//------------------------------------------------------------------
+//_____how you win/lose_____________________________________________
 //(playerWin = playerScore === cpuPick)
+
 
 
 //if player win - ++ win counter 
 
 
-//cpu picks new number
-
-
-//crystals assigned new value
-
-
-//write new score to html
-
 
 //(playerLoss = playerScore > cpuPick)
+
 
 
 //if player losses ++ loss counter
 
 
+
+//------------------------------------------------------------
+//____restart game___________________________________________
 //cpu picks new number
 
 
-//crystals assigned new value
+
+//crystals assigned new hidden value
+
+
+
+//score counter is reset to 0
+
 
 
 //write new score to html
+
+
+
+
+
+
+
+
+
 
 
 
